ba7bae455b9f774bb838cfc7dc51cd929a8fafe5
Ahammed Unni <ahammedunni@expressbase.com>
2017-09-07 18:17:23 +0530

    Changes for Internal Restricted Service calls - TenantDbFactory - internal constructor

---
+++ Data/EbSolutionConnections.cs
@@ -0,0 +1,28 @@
+﻿using ExpressBase.Common.Connections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.Data
+{
+    public class EbSolutionConnections
+    {
+        public string SolutionId { get; set; }
+
+        public EbTiers EbTier { get; set; }
+
+        public EbObjectsDbConnection ObjectsDbConnection { get; set; }
+
+        public EbDataDbConnection DataDbConnection { get; set; }
+
+        public EbFilesDbConnection EbFilesDbConnection { get; set; }
+
+        public EbLogsDbConnection LogsDbConnection { get; set; }
+
+        public SMTPConnection EmailConnection { get; set; }
+
+        public SMSConnection SMSConnection { get; set; }
+    }
+
+    public interface ITenantDbFactory { }
+}
