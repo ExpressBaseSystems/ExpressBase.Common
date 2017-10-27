0e7d9fadd129036f00e7d9f8f84726a3fe5abbaa
RobyAbraham <robyabraham@expressbase.com>
2017-10-27 21:56:15 +0530

    object modify

--- Objects/EbObject.cs
+++ Objects/EbObject.cs
@@ -27,6 +27,10 @@ namespace ExpressBase.Objects
 
         //public string ChangeLog { get; set; }
 
+        public string VersionNumber { get; set; }
+
+        public string Status { get; set; }
+
         public EbObject() { }
 
         public virtual void BeforeRedisSet() { }
