32b94c25fd7d597eb9ccf249ea082255e40a314f
Ahammed Unni V P <ahammedunni@expressbase.com>
2018-01-06 14:29:51 +0530

    Routing Constants

---
+++ DomainConstants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common
+{
+    public class DomainConstants
+    {
+        public const char DOT = '.';
+        public const char BACKSLASH = '/';
+
+        public const string LOCALHOST = "localhost";
+        public const string WWWDOT = "www.";
+        public const string EXPRESSBASEDOTCOM = "expressbase.com";
+        public const string EBTESTINFO = "eb-test.info";
+
+        //Controllers
+        public const string CONTROLLER = "controller";
+        public const string EXTCONTROLLER = "Ext";
+        public const string EB_PRODUCTS = "EbProducts";
+
+        //Actions
+        public const string ACTION = "action";
+        public const string INDEX = "Index";
+        public const string USERSIGNIN2UC = "UsrSignIn";
+
+
+        public const int HOSTPARTSLEN_IS_3 = 3;
+        public const int HOSTPARTSLEN_IS_2 = 2;
+
+    }
+}
---
+++ Enums/DomainEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.Enums
+{
+    public enum SubDomains
+    {
+        forms,
+        pdfreports,
+        bi,
+        bots
+    }
+}
