8e7310136aeac4b40159a8e53a31e16206465a28
Ahammed Unni V P <ahammedunni@expressbase.com>
2018-01-24 18:15:58 +0530

    Env variable Constants

---
+++ EnvironmentConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common
+{
+    public class EnvironmentConstants
+    {
+        public const string SERVICESTACKEXTURL = "ServiceStackExtUrl";
+        public const string SERVICESTACKINTURL = "ServiceStackIntUrl";
+
+        public const string REDISSERVER = "RedisServer";
+        public const string REDISPORT = "RedisPort";
+        public const string REDISPASSWORD = "RedisPassword";
+
+
+    }
+}
