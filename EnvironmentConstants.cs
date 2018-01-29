8a739f6334af2966a24be7841ab6df9aa40ae129
Ahammed Unni V P <ahammedunni@expressbase.com>
2018-01-29 20:55:57 +0530

    Env Constants SS

--- EnvironmentConstants.cs
+++ EnvironmentConstants.cs
@@ -6,12 +6,35 @@ namespace ExpressBase.Common
 {
     public class EnvironmentConstants
     {
+        public const string EB_INFRASTRUCTURE= "EB_INFRASTRUCTURE";
+
         public const string SERVICESTACK_EXT_URL = "SERVICESTACK_EXT_URL";
         public const string SERVICESTACK_INT_URL = "SERVICESTACK_INT_URL";
 
         public const string REDIS_SERVER = "REDIS_SERVER";
         public const string REDIS_PORT = "REDIS_PORT";
         public const string REDIS_PASSWORD = "REDIS_PASSWORD";
+
+        public const string INFRA_DB_SERVER = "INFRA_DB_SERVER";
+        public const string INFRA_DB_PORT = "INFRA_DB_PORT";
+        public const string INFRA_DB_TIMEOUT = "INFRA_DB_TIMEOUT";
+
+        public const string INFRA_DB_RO_USER = "INFRA_DB_RO_USER";
+        public const string INFRA_DB_RO_PASSWORD = "INFRA_DB_RO_PASSWORD";
+        public const string INFRA_DB_RW_USER = "INFRA_DB_RW_USER";
+        public const string INFRA_DB_RW_PASSWORD = "INFRA_DB_RW_PASSWORD";
+
+        public const string INFRA_FILES_DB_URL = "INFRA_FILES_DB_URL";
+
+        public const string JWT_PRIVATE_KEY_XML = "JWT_PRIVATE_KEY_XML";
+        public const string JWT_PUBLIC_KEY_XML = "JWT_PUBLIC_KEY_XML";
+
+        public const string RABBIT_HOST = "RABBIT_HOST";
+        public const string RABBIT_PORT = "RABBIT_PORT";
+        public const string RABBIT_USER = "RABBIT_USER";
+        public const string RABBIT_PASSWORD = "RABBIT_PASSWORD";
+        public const string RABBIT_VHOST = "RABBIT_VHOST";
+
 
 
     }
