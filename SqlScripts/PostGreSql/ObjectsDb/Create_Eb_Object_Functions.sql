115a33d191deec47e25cfa249e2717acc06102fd
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-02-09 19:25:59 +0530

    eb_authenticate_anonymous add

--- SqlScripts/PostGreSql/ObjectsDb/Create_Eb_Object_Functions.sql
+++ SqlScripts/PostGreSql/ObjectsDb/Create_Eb_Object_Functions.sql
@@ -989,3 +989,95 @@ ALTER FUNCTION public.eb_getroles_test(integer, text)
 
 
 
+
+
+-- FUNCTION: public.eb_authenticate_anonymous(text, text, text, text)
+
+-- DROP FUNCTION public.eb_authenticate_anonymous(text, text, text, text);
+
+CREATE OR REPLACE FUNCTION public.eb_authenticate_anonymous(
+	_socialid text DEFAULT NULL::text,
+	_emailid text DEFAULT NULL::text,
+	_phone text DEFAULT NULL::text,
+	_wc text DEFAULT NULL::text)
+    RETURNS TABLE(_userid integer, _email text, _firstname text, _roles_a text, _rolename_a text, _permissions text) 
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 1000
+AS $BODY$
+
+DECLARE _userid INTEGER;
+DECLARE _email TEXT;
+DECLARE _firstname TEXT;
+DECLARE _roles_a TEXT;
+DECLARE _rolename_a TEXT;
+DECLARE _permissions TEXT;
+DECLARE _is_anon_auth_req BOOL;
+
+BEGIN
+
+_is_anon_auth_req := false;
+
+IF _socialid IS NOT NULL THEN
+
+    SELECT userid, email, firstname, roles_a, rolename_a, permissions 
+    FROM eb_authenticate_unified(social => _socialid, wc => _wc) 
+    INTO _userid, _email, _firstname, _roles_a, _rolename_a, _permissions;
+    
+    IF _userid IS NULL THEN
+    
+		SELECT A.id, A.email, A.name FROM eb_usersanonymous A WHERE A.socialid = _socialid 
+        INTO _userid, _email, _firstname;
+            
+		IF _userid IS NULL THEN
+        	INSERT INTO eb_usersanonymous (socialid) VALUES (_socialid);
+		END IF;
+       
+        _is_anon_auth_req := TRUE;
+        
+    END IF;
+
+ELSE
+
+	IF _emailid IS NOT NULL OR _phone IS NOT NULL THEN
+    
+    	SELECT A.id, A.email, A.name FROM eb_usersanonymous A 
+        WHERE A.email = _emailid OR A.phoneno = _phone INTO _userid, _email, _firstname;
+        
+        IF _userid IS NULL THEN
+        	INSERT INTO eb_usersanonymous (email, phoneno) VALUES (_emailid, _phone);
+        ELSE
+        	IF _email IS NULL THEN
+            	UPDATE eb_usersanonymous SET email = _emailid WHERE phoneno = _phone;
+            ELSE
+            	UPDATE eb_usersanonymous SET phoneno = _phone WHERE email = _emailid;
+            END IF;
+        END IF;
+        
+        _is_anon_auth_req := TRUE;
+        
+    END IF;
+    
+END IF;
+
+IF _is_anon_auth_req THEN
+	SELECT userid, email, firstname, roles_a, rolename_a, permissions 
+    FROM eb_authenticate_unified(uname => 'anonymous', password => '294de3557d9d00b3d2d8a1e6aab028cf', wc => _wc) 
+    INTO _userid, _email, _firstname, _roles_a, _rolename_a, _permissions;
+END IF;
+
+RETURN QUERY
+    SELECT _userid, _email, _firstname, _roles_a, _rolename_a, _permissions;
+
+END;
+
+$BODY$;
+
+ALTER FUNCTION public.eb_authenticate_anonymous(text, text, text, text)
+    OWNER TO postgres;
+
+
+
+
