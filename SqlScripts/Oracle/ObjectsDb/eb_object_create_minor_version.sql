1e296b46c7602135fe88aff606436c92a71ef80e
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2018-02-16 14:14:00 +0530

    Objects Functions Migration to oracle

---
+++ SqlScripts/Oracle/ObjectsDb/eb_object_create_minor_version.sql
@@ -0,0 +1,60 @@
+-- FUNCTION: public.eb_objects_create_minor_version(text, integer, integer, text, text, text)
+
+-- DROP FUNCTION public.eb_objects_create_minor_version(text, integer, integer, text, text, text);
+
+
+
+CREATE OR REPLACE FUNCTION eb_object_create_minor_version(
+	idv VARCHAR,
+	obj_typev CLOB,
+	commit_uidv NUMBER,
+	src_pid CLOB,
+	cur_pid CLOB,
+	relationsv VARCHAR)
+    RETURN CLOB
+    AS
+     refidunique VARCHAR(500); inserted_objid CLOB; inserted_obj_ver_id NUMBER; objid NUMBER; committed_refidunique CLOB; 
+minor NUMBER;major NUMBER; version_number CLOB; objjson CLOB;
+
+BEGIN
+	
+	SELECT eb_objects_id, major_ver_num,obj_json into objid, major,objjson FROM eb_objects_ver WHERE refid=idv;
+    
+	SELECT MAX(minor_ver_num) into minor FROM eb_objects_ver WHERE eb_objects_id = objid AND major_ver_num = major;
+    
+    version_number := CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(major,'.'),minor+1),'.'),0),'.'),'w');
+    
+    INSERT INTO 
+		eb_objects_ver (eb_objects_id, obj_json,commit_ts,commit_uid,version_num,working_mode,major_ver_num,minor_ver_num,patch_ver_num) values (objid,objjson,SYSTIMESTAMP,commit_uidv,version_number,'T',major,minor+1,0) 
+        RETURNING id INTO inserted_obj_ver_id;
+    
+    refidunique :=CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(src_pid,'-'),cur_pid),'-'),obj_typev),'-'),objid),'-'),inserted_obj_ver_id);
+    committed_refidunique:=refidunique;
+     
+	UPDATE eb_objects_ver SET refid = refidunique WHERE id = inserted_obj_ver_id;
+    
+    INSERT INTO eb_objects_status(eb_obj_ver_id, status, userid, ts) VALUES(inserted_obj_ver_id, 0, commit_uidv, SYSTIMESTAMP);
+    
+    
+    UPDATE eb_objects_relations SET eb_del = 'T', removed_by= commit_uidv , removed_at=SYSTIMESTAMP
+      WHERE 
+        dominant IN(SELECT dominant FROM eb_objects_relations WHERE dependant = refidunique 
+                    MINUS 
+                    SELECT regexp_substr(relationsv,'[^,]+', 1, level)  AS dominantvals from dual CONNECT BY regexp_substr(relationsv, '[^,]+', 1, level) is not null);
+          
+    
+         INSERT INTO eb_objects_relations 
+        (dominant, dependant) 
+    SELECT 
+      dominantvals, refidunique 
+      FROM (
+      SELECT regexp_substr(relationsv,'[^,]+', 1, level)  AS dominantvals from dual CONNECT BY regexp_substr(relationsv, '[^,]+', 1, level) is not null
+      MINUS
+      select dominant from eb_objects_relations WHERE dependant = refidunique
+      );
+                                   
+    RETURN committed_refidunique; 	
+
+END;
+
+
---
+++ SqlScripts/Oracle/ObjectsDb/eb_objects_exploreobject.sql
@@ -0,0 +1,109 @@
+
+create or replace type obj_explore_record as object (
+  idval number,
+  nameval CLOB,
+  typeval number, 
+  statusval number, 
+  descriptionval CLOB,
+  changelogval CLOB,
+  commitatval TIMESTAMP,
+  commitbyval CLOB,
+  refidval CLOB,
+  ver_numval CLOB,
+  work_modeval char,
+  workingcopiesval CLOB,
+  json_wcval CLOB,
+  json_lcval CLOB,
+  major_verval number,
+  minor_verval number,
+  patch_verval number,
+  tagsval CLOB, 
+  app_idval CLOB,
+  lastversionrefidval CLOB,
+  lastversionnumberval CLOB,
+  lastversioncommit_tsval TIMESTAMP,
+  lastversion_statusvalv number,
+  lastversioncommit_byname CLOB,
+  lastversioncommit_byid number,
+  liveversionrefidval CLOB,
+  liveversionnumberval CLOB,
+  liveversioncommit_tsval TIMESTAMP,
+  liveversion_statusval number,
+  liveversioncommit_byname CLOB,
+  liveversioncommit_byid number,
+  owner_uidval number,
+  owner_tsval TIMESTAMP,
+  owner_nameval CLOB 
+);
+/
+
+create or replace type obj_explore_table as table of obj_explore_record;
+/
+
+
+CREATE OR REPLACE FUNCTION eb_objects_exploreobject(
+	id_ integer)
+    RETURN obj_explore_table as explore_obj obj_explore_table :=obj_explore_table();
+    --TABLE(idval integer,  text, typeval integer, statusval integer, descriptionval text, changelogval text, commitatval text, commitbyval text, refidval text, ver_numval text, work_modeval boolean, workingcopiesval text[], json_wcval json, json_lcval json, major_verval integer, minor_verval integer, patch_verval integer, tagsval text, app_idval text, lastversionrefidval text, lastversionnumberval text, lastversioncommit_tsval text, lastversion_statusvalv integer, lastversioncommit_byname text, lastversioncommit_byid integer, liveversionrefidval text, liveversionnumberval text, liveversioncommit_tsval text, liveversion_statusval integer, liveversioncommit_byname text, liveversioncommit_byid integer, owner_uidval integer, owner_tsval text, owner_nameval text) 
+   
+	workingcopiesval CLOB;
+	json_wcval CLOB; json_lcval CLOB;
+	idval number; nameval CLOB; typeval number; statusval number;
+	descriptionval CLOB; changelogval CLOB; commitatval TIMESTAMP; commitbyval CLOB; refidval CLOB; ver_numval CLOB; work_modeval char;
+	major_verval number; minor_verval number; patch_verval number; tagsval CLOB; app_idval CLOB;
+	lastversionrefidval CLOB; lastversionnumberval CLOB; lastversioncommit_tsval TIMESTAMP; lastversion_statusval number; lastversioncommit_byname CLOB;lastversioncommit_byid number;
+	liveversionrefidval CLOB; liveversionnumberval CLOB; liveversioncommit_tsval TIMESTAMP; liveversion_statusval number; liveversioncommit_byname CLOB;liveversioncommit_byid number;
+	owner_uidVal number; owner_tsVal TIMESTAMP; owner_nameVal CLOB;
+ 
+BEGIN
+
+--Live version details
+SELECT
+    EOV.version_num, EOV.refid, EOV.commit_ts, EOS.status, EOV.commit_uid, EU.firstname INTO
+	liveversionnumberval, liveversionrefidval, liveversioncommit_tsval, liveversion_statusval,liveversioncommit_byid,liveversioncommit_byname
+FROM
+    eb_objects_ver EOV, eb_objects_status EOS, eb_objects EO, eb_users EU
+WHERE
+    EO.id = id_ AND EOV.eb_objects_id = id_ AND EOS.status = 3 AND EOS.eb_obj_ver_id = EOV.id AND EOV.commit_uid = EU.id;
+	
+--Latest commited vaersion details
+SELECT
+    EOV.version_num, EOV.refid, EOV.commit_ts, EOS.status, EU.firstname,  EOV.commit_uid
+    INTO lastversionnumberval, lastversionrefidval, lastversioncommit_tsval, lastversion_statusval, lastversioncommit_byname, lastversioncommit_byid
+FROM
+    eb_objects_ver EOV, eb_objects_status EOS, eb_users EU
+WHERE
+    EOV.eb_objects_id = id_ AND EOS.eb_obj_ver_id = EOV.id AND EOV.commit_uid = EU.id AND COALESCE(EOV.working_mode,'F') = 'F'
+    AND ROWNUM<=1 ORDER BY commit_ts DESC;
+
+-- Owner details
+SELECT  
+	EO.owner_uid, EO.owner_ts, EU.firstname
+INTO 
+	owner_uidVal, owner_tsVal, owner_nameVal
+FROM 
+	eb_objects EO, eb_users EU
+WHERE 
+	EO.id = id_ AND EU.id = EO.owner_uid;
+	
+SELECT idv, namev, typev, status, description, changelog, commitat, commitby, refidv, ver_num, COALESCE(work_mode,'F'), workingcopies,
+		json_wc, json_lc, major_ver, minor_ver, patch_ver, tags, app_id 
+INTO
+		idval, nameval, typeval, statusval, descriptionval, changelogval, commitatval, commitbyval, refidval,
+		ver_numval, work_modeval, workingcopiesval,
+		json_wcval, json_lcval, major_verval, minor_verval, patch_verval, tagsval, app_idval
+    FROM 
+		table(eb_objects_getversiontoopen(id_)) ;
+
+explore_obj.extend();
+explore_obj(1):=obj_explore_record(
+idval, nameval, typeval, statusval, descriptionval, changelogval, commitatval, commitbyval, refidval, 
+	ver_numval, work_modeval, workingcopiesval, json_wcval, json_lcval, major_verval, minor_verval, patch_verval,
+	tagsval, app_idval,
+    lastversionrefidval, lastversionnumberval, lastversioncommit_tsval, lastversion_statusval, lastversioncommit_byname,lastversioncommit_byid,
+	liveversionrefidval, liveversionnumberval, liveversioncommit_tsval, liveversion_statusval, liveversioncommit_byname,liveversioncommit_byid,
+	owner_uidVal, owner_tsVal, owner_nameVal);
+RETURN explore_obj;	
+END;
+
+
