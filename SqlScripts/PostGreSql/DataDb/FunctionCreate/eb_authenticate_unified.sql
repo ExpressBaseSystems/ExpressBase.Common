3e19ed0f523b98b83a47c632140ffb207b6c0ad0
Bini Varghese <binivarghese@expressbase.com>
2018-03-12 11:48:55 +0530

    functions modified

--- SqlScripts/PostGreSql/DataDb/FunctionCreate/eb_authenticate_unified.sql
+++ SqlScripts/PostGreSql/DataDb/FunctionCreate/eb_authenticate_unified.sql
@@ -7,10 +7,12 @@ CREATE OR REPLACE FUNCTION public.eb_authenticate_unified(
 	password text DEFAULT NULL::text,
 	social text DEFAULT NULL::text,
 	wc text DEFAULT NULL::text)
-    RETURNS TABLE(userid integer, email text, fullname text, roles_a text, rolename_a text, permissions text) 
+    RETURNS TABLE(userid integer, email text, fullname text, roles_a text, rolename_a text, permissions text, preferencesjson text) 
     LANGUAGE 'plpgsql'
 
-   
+    COST 100
+    VOLATILE 
+    ROWS 1000
 AS $BODY$
 
 DECLARE userid INTEGER;
@@ -19,22 +21,23 @@ DECLARE fullname TEXT;
 DECLARE roles_a TEXT;
 DECLARE rolename_a TEXT;
 DECLARE permissions TEXT;
+DECLARE preferencesjson TEXT;
 
 BEGIN
 	-- NORMAL
 	IF uname IS NOT NULL AND password IS NOT NULL AND social IS NULL THEN
-        SELECT eb_users.id, eb_users.email, eb_users.fullname
-        FROM eb_users WHERE eb_users.email = uname AND pwd = password INTO userid, email, fullname;
+        SELECT eb_users.id, eb_users.email, eb_users.fullname, eb_users.preferencesjson
+        FROM eb_users WHERE eb_users.email = uname AND pwd = password INTO userid, email, fullname, preferencesjson;
     END IF;
     -- SSO
     IF uname IS NOT NULL AND password IS NULL AND social IS NULL THEN
-        SELECT eb_users.id, eb_users.email, eb_users.fullname
-        FROM eb_users WHERE eb_users.email = uname INTO userid, email, fullname;
+        SELECT eb_users.id, eb_users.email, eb_users.fullname, eb_users.preferencesjson
+        FROM eb_users WHERE eb_users.email = uname INTO userid, email, fullname, preferencesjson;
     END IF;
     -- SOCIAL
     IF uname IS NULL AND password IS NULL AND social IS NOT NULL THEN
-        SELECT eb_users.id, eb_users.email, eb_users.fullname
-        FROM eb_users WHERE eb_users.socialid = social INTO userid, email, fullname;
+        SELECT eb_users.id, eb_users.email, eb_users.fullname, eb_users.preferencesjson
+        FROM eb_users WHERE eb_users.socialid = social INTO userid, email, fullname, preferencesjson;
     END IF;
 
 	IF userid > 0 THEN
@@ -42,7 +45,7 @@ BEGIN
 
         SELECT eb_getpermissions(string_to_array(roles_a, ',')::int[]) INTO permissions;
 
-        RETURN QUERY SELECT userid, email, fullname, roles_a, rolename_a, permissions;
+        RETURN QUERY SELECT userid, email, fullname, roles_a, rolename_a, permissions, preferencesjson;
    	END IF;
 END;
 
