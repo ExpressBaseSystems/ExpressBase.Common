2be48ebdc4d33d8095ae79fa15d7719a0a7e8479
Bini Varghese <binivarghese@expressbase.com>
2018-03-22 10:59:23 +0530

    new function query for assign privileges

---
+++ SqlScripts/PostGreSql/DataDb/FunctionCreate/eb_assignprivileges.sql
@@ -0,0 +1,32 @@
+﻿-- FUNCTION: public.eb_assignprivileges(text, text, text, text, text, text)
+
+-- DROP FUNCTION public.eb_assignprivileges(text, text, text, text, text, text);
+
+CREATE OR REPLACE FUNCTION public.eb_assignprivileges(
+	unameadmin text,
+	passwordadmin text,
+	unamerouser text,
+	passwordro text,
+	unamerwuser text,
+	passwordrw text)
+    RETURNS text
+    LANGUAGE 'plpgsql'
+
+AS $BODY$
+
+ 
+BEGIN
+   EXECUTE 'CREATE ROLE '|| unameadmin ||' WITH LOGIN PASSWORD '''|| passwordAdmin ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
+ 
+   EXECUTE 'CREATE ROLE '|| unameROUser ||' WITH LOGIN PASSWORD '''|| passwordRO ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
+   
+   EXECUTE 'CREATE ROLE '|| unameRWUser ||' WITH LOGIN PASSWORD '''|| passwordRW ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
+      
+   RETURN 'T';
+END;
+
+$BODY$;
+
+ALTER FUNCTION public.eb_assignprivileges(text, text, text, text, text, text)
+    OWNER TO postgres;
+
