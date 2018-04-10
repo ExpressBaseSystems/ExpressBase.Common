896b3baffd07968267850046e932588c38e4d0e5
Bini Varghese <binivarghese@expressbase.com>
2018-04-10 12:47:57 +0530

    minor in eb_getroles script

--- SqlScripts/ORACLE/DataDB/FunctionCreate/eb_getroles.sql
+++ SqlScripts/ORACLE/DataDB/FunctionCreate/eb_getroles.sql
@@ -1,4 +1,3 @@
-
 create or replace FUNCTION eb_getroles(
 	userid number,
     wc1 VARCHAR DEFAULT NULL)
@@ -17,29 +16,29 @@ create or replace FUNCTION eb_getroles(
          l_numbers := apptype(NULL);
     END IF;
     SELECT 
-    	tblroleobj(LISTAGG(qury.role_id,',') within group(order by qury.role_id),LISTAGG(CASE WHEN r.role_name is NULL THEN 'SYS' ELSE r.role_name END,',') within group(order by qury.role_id)) BULK COLLECT INTO roles_tbl  FROM 
+    	tblroleobj(LISTAGG(role_id,',') within group(order by role_id),LISTAGG(CASE WHEN UROLES.role_name is NULL THEN 'SYS' ELSE UROLES.role_name END,',') within group(order by role_id)) BULK COLLECT INTO roles_tbl FROM 
+         (SELECT role_id, r.role_name, r.applicationid FROM       
          (SELECT DISTINCT role_id FROM
 				(
                 WITH role2role(role_id) AS 
                  (
 					SELECT 
 						role2_id AS roleid FROM eb_role2role
-					WHERE role1_id = ANY(SELECT role_id FROM eb_role2user WHERE user_id = userid AND eb_del = 'F')
+					WHERE role1_id = ANY(SELECT role_id FROM eb_role2user WHERE user_id = 2 AND eb_del = 'F')
 					UNION ALL
 					SELECT e.role2_id FROM eb_role2role e, role2role r WHERE e.role1_id = r.role_id AND eb_del='F'
 				  ) SELECT * FROM role2role  
                   UNION ALL
                  SELECT 
 					role_id FROM eb_role2user 
-		 		WHERE user_id = userid AND eb_del = 'F') ,eb_roles
-				WHERE (id = role_id 
-					AND	applicationid = ANY(SELECT eb_applications.id FROM eb_applications WHERE application_type=ANY(select * from table(l_numbers))) 
-					AND	eb_del = 'F') OR role_id < 100 ORDER BY role_id) qury                    
-                    LEFT JOIN 
-                    (
-                        SELECT id,eb_roles.role_name FROM eb_roles 
-                            WHERE eb_del = 'F' ORDER BY id 
-                    )  r 
-                    ON qury.role_id = r.id;
+		 		WHERE user_id = 2 AND eb_del = 'F')) qury
+		      LEFT JOIN             
+               (SELECT * FROM eb_roles WHERE eb_del = 'F' AND applicationid = ANY(SELECT eb_applications.id FROM eb_applications WHERE application_type=ANY(select * from table(l_numbers)))) r
+		ON
+			qury.role_id = r.id
+		) UROLES
+		WHERE 
+			role_id < 100 OR 
+			applicationid IS NOT NULL ;
     RETURN roles_tbl;
 END;
