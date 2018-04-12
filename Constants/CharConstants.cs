a013fa24f094486c2f4811b4a2d8b0076d2624fc
Ahammed Unni V P <ahammedunni@expressbase.com>
2018-04-12 15:01:09 +0530

    File Constants

---
+++ Constants/CharConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpressBase.Common.Constants
+{
+    public class CharConstants
+    {
+        public const char DOT = '.';
+        public const char COMMA = ',';
+        public const char BACKSLASH = '/';
+        public const char DASH = '-';
+        public const char UNDERSCORE = '_';
+
+    }
+}
---
+++ Constants/StaticFileConstants.cs
@@ -0,0 +1,39 @@
+﻿namespace ExpressBase.Common.Constants
+{
+    public class StaticFileConstants
+    {
+        // File Name Parts
+        public const string LOGO = "logo";
+        public const string DP = "dp";
+        public const string SMALL = "small";
+        public const string MEDIUM = "medium";
+        public const string LARGE = "large";
+
+        //Bucket Names
+        public const string EXTERNAL = "external";
+        public const string SOL_LOGOS = "sol_logos";
+        public const string DP_IMAGES = "dp_images";
+        public const string IMAGES_ORIGINAL = "images_original";
+        public const string FILES = "files";
+        public const string IMAGES = "images";
+        public const string IMAGES_SMALL = "images_small";
+        public const string IMAGES_MEDIUM = "images_medium";
+        public const string IMAGES_LARGE = "images_large";
+
+        //Notification Message Constants
+        public const string UPLOADSUCCESS = "cmd.onUploadSuccess";
+
+        //FileTypes
+        public const string PNG = "png";
+        public const string PDF = "pdf";
+        public const string JPG = "jpg";
+
+        public const string DOTPNG = ".png";
+        public const string DOTPDF = ".pdf";
+        public const string DOTJPG = ".jpg";
+
+        //Tags
+        public const string TAGS = "Tags";
+
+    }
+}
