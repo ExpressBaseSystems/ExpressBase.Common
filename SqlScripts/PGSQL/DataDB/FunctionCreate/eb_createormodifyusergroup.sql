20fb39f9fa8ca52d8e1f9ced3b402719b19a7f22
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-05-08 20:09:35 +0530

    eb_createormodifyusergroup int arr replaced with text

--- SqlScripts/PGSQL/DataDB/FunctionCreate/eb_createormodifyusergroup.sql
+++ SqlScripts/PGSQL/DataDB/FunctionCreate/eb_createormodifyusergroup.sql
@@ -1,37 +1,41 @@
--- FUNCTION: public.eb_createormodifyusergroup(integer, integer, text, text, integer[])
+-- FUNCTION: public.eb_createormodifyusergroup(integer, integer, text, text, text)
 
--- DROP FUNCTION public.eb_createormodifyusergroup(integer, integer, text, text, integer[]);
+-- DROP FUNCTION public.eb_createormodifyusergroup(integer, integer, text, text, text);
 
 CREATE OR REPLACE FUNCTION public.eb_createormodifyusergroup(
 	_userid integer,
 	_id integer,
 	_name text,
 	_description text,
-	_users integer[])
+	_users text)
     RETURNS TABLE(gid integer) 
     LANGUAGE 'plpgsql'
 
-    
+    COST 100
+    VOLATILE 
+    ROWS 1000
 AS $BODY$
 
 DECLARE gid integer;
+users INTEGER[];
 BEGIN
 gid:=_id;
+users := string_to_array(_users, ',')::integer[];
 IF _id > 0 THEN
 
 UPDATE eb_usergroup SET name=_name, description=_description WHERE id=_id;
 INSERT INTO eb_user2usergroup(userid,groupid,createdby,createdat) SELECT userid,_id,_userid,NOW() FROM 
-   UNNEST(array(SELECT unnest(_users) except 
+   UNNEST(array(SELECT unnest(users) except 
 		SELECT UNNEST(array(SELECT userid from eb_user2usergroup WHERE groupid = _id AND eb_del = 'F')))) AS userid;
 UPDATE eb_user2usergroup SET eb_del = 'T',revokedby = _userid,revokedat =NOW() WHERE userid IN(
 	SELECT UNNEST(array(SELECT userid from eb_user2usergroup WHERE groupid = _id AND eb_del = 'F')) except 
-	SELECT UNNEST(_users));
+	SELECT UNNEST(users));
 
 ELSE
 
 INSERT INTO eb_usergroup (name,description,eb_del) VALUES (_name,_description,'F') returning id INTO gid;
 INSERT INTO eb_user2usergroup(userid,groupid,createdby,createdat) SELECT userid, gid,_userid,NOW() 
-    FROM UNNEST(_users) AS userid;
+    FROM UNNEST(users) AS userid;
 
 END IF;
 
@@ -41,6 +45,8 @@ END;
 
 $BODY$;
 
-ALTER FUNCTION public.eb_createormodifyusergroup(integer, integer, text, text, integer[])
+ALTER FUNCTION public.eb_createormodifyusergroup(integer, integer, text, text, text)
     OWNER TO postgres;
+
+
 
