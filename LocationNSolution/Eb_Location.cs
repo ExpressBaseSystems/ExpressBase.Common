5b06bc2edea2a5c371b02ee571956a81f4047c8e
DonajoseUllattil <donajose@expressbase.com>
2018-06-11 17:41:02 +0530

    Location related classes -

---
+++ LocationNSolution/Eb_Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.LocationNSolution
+{
+    class Eb_Location
+    {
+        public int LocId { get; set; }
+
+        public string LongName { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string Meta { get; set; }
+
+        public string Img { get; set; }
+    }
+}
