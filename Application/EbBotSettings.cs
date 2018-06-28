4395f51bf00a430bc91431968da6ff69a5e9dcab
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-06-28 11:21:00 +0530

    EbBot App Settings class created

---
+++ Application/EbBotSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExpressBase.Objects.ObjectContainers
+{
+	[DataContract]
+	public class EbBotSettings
+	{
+		[DataMember(Order = 1)]
+		public string WelcomeMessage { get; set; }
+
+		[DataMember(Order = 2)]
+		public string ThemeColor { get; set; }
+
+		[DataMember(Order = 3)]
+		public string DpUrl { get; set; }
+
+		public EbBotSettings(){}
+    }
+}
