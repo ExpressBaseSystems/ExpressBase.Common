54b074b8946ff5d7ca7a918f3fcdd60dcab9bbc9
Jith Job K.I <jithjob@expressbase.com>
2018-06-29 14:01:12 +0530

    ebobjects_change_status converted to sql

---
+++ sqlscripts/mysql/objectsdb/functioncreate/eb_objects_change_status.sql
@@ -0,0 +1,35 @@
+﻿-- FUNCTION: public.eb_objects_change_status(text, integer, integer, text)
+
+-- DROP FUNCTION IF EXISTS  eb_objects_change_status;
+DELIMITER $$
+
+CREATE FUNCTION eb_objects_change_status(
+	idv text,
+	statusv integer,
+	commit_uid integer,
+	changelogv text)
+    RETURNS boolean
+    DETERMINISTIC   
+BEGIN
+
+	DECLARE inserted_obj_ver_id integer;
+	 
+	  
+	INSERT INTO
+		eb_objects_status(eb_obj_ver_id)
+	SELECT
+		id
+	FROM
+		eb_objects_ver
+	WHERE
+		refid=idv;
+	SELECT last_insert_id() INTO inserted_obj_ver_id;
+
+	UPDATE
+		eb_objects_status 
+	SET
+		status = statusv, uid = commit_uid, ts = NOW(), changelog = changelogv
+	WHERE
+		id = inserted_obj_ver_id;
+        RETURN true;
+END$$
