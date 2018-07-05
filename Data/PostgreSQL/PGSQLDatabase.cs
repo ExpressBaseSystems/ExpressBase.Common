8db96c1c1a4bce04d0a961422fa73228c81bef0a
Ahammed Unni <ahammedunni@expressbase.com>
2018-07-05 23:48:43 +0530

    SSL mode disable for AlArz //Temporary

--- Data/PostgreSQL/PGSQLDatabase.cs
+++ Data/PostgreSQL/PGSQLDatabase.cs
@@ -83,7 +83,8 @@ namespace ExpressBase.Common
             }
         }
 
-        private const string CONNECTION_STRING_BARE = "Host={0}; Port={1}; Database={2}; Username={3}; Password={4}; SSL Mode=Require; Use SSL Stream=true; Trust Server Certificate=true; Pooling=true; CommandTimeout={5};";
+        private const string CONNECTION_STRING_BARE = "Host={0}; Port={1}; Database={2}; Username={3}; Password={4};  Trust Server Certificate=true; Pooling=true; CommandTimeout={5};";
+        //SSL Mode=Require; Use SSL Stream=true;
         private string _cstr;
         private EbBaseDbConnection EbBaseDbConnection { get; set; }
 
