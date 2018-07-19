e0abc345a19ac2d1dae8d7e0813e77b22d7ffd1f
Ahammed Unni <ahammedunni@expressbase.com>
2018-07-19 19:16:19 +0530

    Paypal

---
+++ Enums/PayPalEnums.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.Enums
+{
+    public enum HttpMethods
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        HEAD,
+        CONNECT,
+        OPTIONS,
+        PATCH
+    };
+
+    public enum PlanType
+    {
+        FIXED,
+        INFINITE
+    };
+
+    public enum PlanState
+    {
+        CREATED,
+        ACTIVE,
+        INACTIVE
+    };
+
+    public enum TermType
+    {
+        MONTHLY,
+        WEEKLY,
+        YEARLY
+    };
+
+    public enum PaymentMethod
+    {
+        bank,
+        paypal
+    };
+
+    public enum CardState
+    {
+        expired,
+        ok
+    };
+}
