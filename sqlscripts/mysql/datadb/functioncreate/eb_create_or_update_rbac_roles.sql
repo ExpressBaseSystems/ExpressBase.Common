563e682d9621069b5f6907921b498e57208ab319
Jith Job K.I <jithjob@expressbase.com>
2018-07-31 18:34:36 +0530

    eb_create_or_update_rbac_roles correction

--- sqlscripts/mysql/datadb/functioncreate/eb_create_or_update_rbac_roles.sql
+++ sqlscripts/mysql/datadb/functioncreate/eb_create_or_update_rbac_roles.sql
@@ -19,18 +19,6 @@ CREATE FUNCTION eb_create_or_update_rbac_roles(
 BEGIN
 	DECLARE rid integer;
 	DECLARE tmp integer;
-
-	CREATE TEMPORARY TABLE IF NOT EXISTS temp_array_table(value INTEGER);
-        
-	CALL STR_TO_TBL(users);  -- fill to temp_array_table
-	CREATE TEMPORARY TABLE IF NOT EXISTS _users SELECT `value` FROM temp_array_table;
-	
-	CALL STR_TO_TBL(dependantroles);  -- fill to temp_array_table
-	CREATE TEMPORARY TABLE IF NOT EXISTS _dependantroles SELECT `value` FROM temp_array_table;
-    
-	CREATE TEMPORARY TABLE IF NOT EXISTS temp_array_table(value TEXT);
-
-
 	SET rid := roleid;
     
 	IF permissions IS NOT NULL THEN
@@ -41,15 +29,10 @@ BEGIN
 	  END IF;
 	END IF;
 	IF isanonym = 'T' THEN
-		DELETE FROM _users;
-        INSERT INTO _users VALUES(1);    
-		DELETE FROM dependantroles;        
+		SET users := 1;    
+		SET dependantroles :='' ;        
 	END IF;
-	IF _users IS NOT NULL OR roleid > 0 THEN
-		SELECT eb_create_or_update_role2user(rid, userid, _users) INTO tmp;
-	END IF;
-	IF _dependantroles IS NOT NULL OR roleid > 0 THEN
-	   SELECT eb_create_or_update_role2role(rid, userid, _dependantroles) INTO tmp;
-	END IF;    
+	SELECT eb_create_or_update_role2user(rid, userid, users) INTO tmp;
+	SELECT eb_create_or_update_role2role(rid, userid, dependantroles) INTO tmp;
 RETURN 0;
 END;
