89a8a2f3c13a71aea3347f8e810b9f5344e460ff
Jith Job K.I <jithjob@expressbase.com>
2018-09-27 19:33:18 +0530

    TableColumnMeta seperated

---
+++ Objects/TableColumnMeta.cs
@@ -0,0 +1,14 @@
+﻿using ExpressBase.Common.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common
+{
+    public class TableColumnMeta
+    {
+        public string Name { get; set; }
+        public VendorDbType Type { get; set; }
+        public string Default { get; set; }
+    }
+}
