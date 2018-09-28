d9f9c3fb29686e74cbd0917f6a382f902638eb09
Jith Job K.I <jithjob@expressbase.com>
2018-09-28 19:02:02 +0530

    TableColumnMetaS added

--- Objects/TableColumnMeta.cs
+++ Objects/TableColumnMeta.cs
@@ -2,13 +2,37 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace ExpressBase.Common
 {
     public class TableColumnMeta
     {
         public string Name { get; set; }
-        public VendorDbType Type { get; set; }
+        public VendorDbType Type
+        {
+            get { return Type; }
+            set
+            {
+                Type = Type;
+            }
+        }
         public string Default { get; set; }
+    }
+
+    public class TableColumnMetaS
+    {
+        public TableColumnMetaS() { }
+
+        public string Name { get; set; }
+
+        public dynamic Value { get; set; }
+
+        public int Type { get; set; }
+
+        public bool AutoIncrement { get; set; }
+    }
+
+    public class TableColumnMetaSColl : List<TableColumnMetaS>
+    {
+        public TableColumnMetaSColl() { }
     }
 }
