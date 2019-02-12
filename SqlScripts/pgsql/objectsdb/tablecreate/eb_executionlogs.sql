961e8c08c2288422ed9ee4e29faa66d49095ce4f
Dona Jose <donajose@expressbase.com>
2019-02-12 19:39:25 +0530

    table create scripts

---
+++ SqlScripts/pgsql/objectsdb/tablecreate/eb_executionlogs.sql
@@ -0,0 +1,21 @@
+﻿-- Table: public.eb_executionlogs
+
+-- DROP TABLE public.eb_executionlogs;
+
+CREATE TABLE public.eb_executionlogs
+(
+    id integer NOT NULL DEFAULT nextval('executionlogs_id_seq'::regclass),
+    rows character varying COLLATE pg_catalog."default",
+    exec_time integer,
+    created_by integer,
+    created_at timestamp without time zone,
+    refid text COLLATE pg_catalog."default",
+    params json
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.eb_executionlogs
+    OWNER to postgres;
