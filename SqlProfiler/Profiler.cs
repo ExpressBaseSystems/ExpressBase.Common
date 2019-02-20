638b924c0d39eba30c6b4a11494669793dca83d3
Dona Jose <donajose@expressbase.com>
2019-02-20 09:52:14 +0530

    Profiler modifications

--- SqlProfiler/Profiler.cs
+++ SqlProfiler/Profiler.cs
@@ -7,15 +7,34 @@ namespace ExpressBase.Common.SqlProfiler
     public class Profiler
     {
         public int Max_id { get; set; }
+
         public decimal Max_exectime { get; set; }
+
         public int Min_id { get; set; }
+
         public decimal Min_exectime { get; set; }
+
+        public int Cur_Mon_Max_id { get; set; }
+
+        public decimal Cur_Mon_Max_exectime { get; set; }
+
+        public int Cur_Mon_Min_id { get; set; }
+
+        public decimal Cur_Mon_Min_exectime { get; set; }
+
         public int Cur_Max_id { get; set; }
+
         public decimal Cur_Max_exectime { get; set; }
+
         public int Cur_Min_id { get; set; }
+
         public decimal Cur_Min_exectime { get; set; }
+
         public int Total_count { get; set; }
+
         public int Current_count { get; set; }
+
         public int Month_count { get; set; }
+
     }
 }
