36b132e80e3df6dfb7998f2191985a56b0479fad
Dona Jose <donajose@expressbase.com>
2019-03-06 11:06:27 +0530

    Sql profiler modifications

--- SqlProfiler/EbExecutionLogs.cs
+++ SqlProfiler/EbExecutionLogs.cs
@@ -7,12 +7,20 @@ namespace ExpressBase.Common.SqlProfiler
     public class EbExecutionLogs
     {
         public int Id { get; set; }
+
         public string Rows { get; set; }
+
         public decimal Exec_time { get; set; }
+
         public int Created_by { get; set; }
+
+        public string Username { get; set; }
+
         public DateTime Created_at { get; set; }
+
         public string Refid { get; set; }
-        public string Params { get; set; }
+
+        public List<JsonParams> Params { get; set; }
 
     }
 }
---
+++ SqlProfiler/JsonParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.SqlProfiler
+{
+    public class JsonParams
+    {
+            public string Name { get; set; }
+
+            public string Type { get; set; }
+
+            public object Value { get; set; }
+    }
+}
