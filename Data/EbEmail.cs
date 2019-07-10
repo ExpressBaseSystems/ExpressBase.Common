548eef7ca7511e25b88248b715046351b333e9d8
Dona Jose <donajose@expressbase.com>
2019-07-10 11:28:11 +0530

    Minor

--- Data/EbEmail.cs
+++ Data/EbEmail.cs
@@ -8,73 +8,6 @@ using System.Text;
 
 namespace ExpressBase.Common.Data
 {
-    public class EbEmail : IEbConnection
-    {
-        public int Id { get; set; }
-
-        public bool IsDefault { get; set; }
-
-        public string NickName { get; set; }
-
-        public string ProviderName { get; set; }
-
-        public string Host { get; set; }
-
-        public int Port { get; set; }
-
-        public string EmailAddress { get; set; }
-
-        public string Password { get; set; }
-
-        public bool EnableSsl { get; set; }
-
-        public ConPreferences Preference { get; set; }
-
-        public EbConnectionTypes EbConnectionType { get { return EbConnectionTypes.SMTP; } }
-
-        private SmtpClient _client { get; set; }
-
-        public bool Send(string to, string subject, string message, string[] cc, string[] bcc, byte[] attachment, string attachmentname)
-        {
-            _client = new SmtpClient();
-            _client.Host = Host;
-            _client.Port = Port;
-            _client.Credentials = new NetworkCredential { UserName = EmailAddress, Password = Password };
-            _client.EnableSsl = EnableSsl;
-
-            bool sentStatus;
-            try
-            {
-                MailMessage mm = new MailMessage(EmailAddress, to)
-                {
-                    Subject = subject,
-                    IsBodyHtml = true,
-                    Body = message
-
-                };
-                if (attachment != null)
-                    mm.Attachments.Add(new Attachment(new MemoryStream(attachment), attachmentname + ".pdf"));
-                if (cc != null)
-                    if (cc.Length > 0)
-                        foreach (string item in cc)
-                            if (item != "") mm.CC.Add(item);
-                if (bcc != null)
-                    if (bcc.Length > 0)
-                        foreach (string item in bcc)
-                            if (item != "") mm.Bcc.Add(item);
-                _client.Send(mm);
-                sentStatus = true;
-                Console.WriteLine("Smtp Send success" + to);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Smtp Send Exception" + e.Message);
-                sentStatus = false;
-            }
-            return sentStatus;
-        }
-    }
-
     public class EbSmtp
     {
         public EbSmtp(EbSmtpConfig config)
@@ -85,11 +18,13 @@ namespace ExpressBase.Common.Data
                 {
                     Console.WriteLine("EbSmtp Config host,port,address - " + config.Host + "  " + config.Port + "  " + config.EmailAddress);
                     Config = config;
-                    Client = new SmtpClient();
-                    Client.Host = Config.Host;
-                    Client.Port = Config.Port;
-                    Client.Credentials = new NetworkCredential { UserName = Config.EmailAddress, Password = Config.Password };
-                    Client.EnableSsl = Config.EnableSsl;
+                    Client = new SmtpClient
+                    {
+                        Host = Config.Host,
+                        Port = Config.Port,
+                        Credentials = new NetworkCredential { UserName = Config.EmailAddress, Password = Config.Password },
+                        EnableSsl = Config.EnableSsl
+                    };
                 }
                 else
                     Console.WriteLine("ERROR: EbSmtp Config Error........config is null");
