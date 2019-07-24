f7c2b427823f715dd39003913c7f6343396d10f7
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2019-07-24 10:31:51 +0530

    eb_assign_privilages function updated for including special characters

--- SqlScripts/infradb/functioncreate/eb_assignprivileges.sql
+++ SqlScripts/infradb/functioncreate/eb_assignprivileges.sql
@@ -15,7 +15,7 @@ AS $BODY$
    passwordAdmin TEXT := '';
    DECLARE passwordRO TEXT := '';
    DECLARE passwordRW TEXT := '';
-   chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+   chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,!,@,#,$,%,&,*}';
    i integer := 0;
  
 BEGIN
@@ -25,12 +25,14 @@ BEGIN
         passwordRO := passwordRO || chars[1+random()*(array_length(chars, 1)-1)];
         passwordRW := passwordRW || chars[1+random()*(array_length(chars, 1)-1)];
     end loop;
+   
    EXECUTE 'CREATE ROLE '|| unameadmin ||' WITH LOGIN PASSWORD '''|| passwordAdmin ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
  
-   EXECUTE 'CREATE ROLE '|| unameROUser ||' WITH LOGIN PASSWORD '''|| passwordRO ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
+   EXECUTE 'CREATE ROLE '|| unamerouser ||' WITH LOGIN PASSWORD '''|| passwordRO ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
    
-   EXECUTE 'CREATE ROLE '|| unameRWUser ||' WITH LOGIN PASSWORD '''|| passwordRW ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';
-      
+   EXECUTE 'CREATE ROLE '|| unamerwuser ||' WITH LOGIN PASSWORD '''|| passwordRW ||''' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION  VALID UNTIL ''infinity''';  
+	
+   
    RETURN QUERY SELECT passwordAdmin,passwordRO,passwordRW ;
 END;
 
@@ -38,4 +40,3 @@ $BODY$;
 
 ALTER FUNCTION public.eb_assignprivileges(text, text, text)
     OWNER TO postgres;
-
