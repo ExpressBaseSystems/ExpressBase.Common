86bdaabcc686e84923bc7d1b705c86f67d270b6a
Jith Job <jithjob123@hotmail.com>
2019-08-22 20:52:50 +0530

    const CONT_PROP_BTN added

--- Helpers/HtmlConstants.cs
+++ Helpers/HtmlConstants.cs
@@ -7,6 +7,7 @@ namespace ExpressBase.Common
     public static class HtmlConstants
     {
         public const string Label = @"";
+        public const string CONT_PROP_BTN = @"<div class='cont-prop-btn'><i class='fa fa-ellipsis-v' aria-hidden='true'></i></div>";
         public const string TOOL_HTML = @"
 <div class='tool_item_head' data-toggle='collapse' data-target='#@id@'><i class='fa fa-caret-down'></i> @label@ </div>
     <div id='@id@' ebclass='tool-sec-cont' class='tool-sec-cont collapse in'>";
@@ -15,7 +16,7 @@ namespace ExpressBase.Common
 
         public const string CONTROL_WRAPER_HTML4WEB = @"
         <div id='cont_@ebsid@' ebsid='@ebsid@' name='@name@' class='Eb-ctrlContainer' @childOf@ ctype='@type@' eb-hidden='@isHidden@'>
-            <span class='eb-ctrl-label' ui-label id='@ebsidLbl'>@Label@</span>
+            <span class='eb-ctrl-label eb-label-editable' ui-label id='@ebsidLbl'>@Label@</span>
             <input id='@ebsid@lbltxtb' class='eb-lbltxtb' type='text'/> @req@ 
                 <div  id='@ebsid@Wraper' class='ctrl-cover'>
                     @barehtml@
