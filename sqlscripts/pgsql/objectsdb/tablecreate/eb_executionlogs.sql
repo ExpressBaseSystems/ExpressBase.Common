fc74b801510a94939ef6f37260eadf848e1a9360
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2019-11-04 15:46:40 +0530

    eb_executionlog script added

---
+++ sqlscripts/pgsql/objectsdb/tablecreate/eb_executionlogs.sql
@@ -0,0 +1,15 @@
+-- Table: public.eb_executionlogs
+
+-- DROP TABLE public.eb_executionlogs;
+
+CREATE TABLE eb_executionlogs
+(
+    id serial,
+    rows text,
+    exec_time integer,
+    created_by integer,
+    created_at timestamp without time zone,
+    refid text,
+    params json,
+	CONSTRAINT eb_executionlogs_pkey PRIMARY KEY (id)
+);
