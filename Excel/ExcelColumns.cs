0a8c59e05c2edcc70d19f043da25af544e989d64
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2020-02-21 12:06:43 +0530

    Excel

---
+++ Excel/ExcelColumns.cs
@@ -0,0 +1,28 @@
+﻿using ExpressBase.Common.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.Excel 
+{
+   //public class ExcelColumns
+   // {
+   //     public string Name { get; set; }
+
+   //     //public string Label { get; set; }
+
+   //     //public EbDbTypes DbType { get; set; }
+
+   //     public string TableName { get; set; }
+   // }
+    public class ColumnsInfo
+    {
+        public string Name { get; set; }
+
+        public string Label { get; set; }
+
+        public EbDbTypes DbType { get; set; }
+
+        public string TableName { get; set; }
+    }
+}
