9424ae27169b7bc1c53f564c75e78474b969afcb
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2020-04-06 19:09:28 +0530

    eb_locations & eb_location_types script updated

--- sqlscripts/mysql/objectsdb/tablecreate/eb_locations.sql
+++ sqlscripts/mysql/objectsdb/tablecreate/eb_locations.sql
@@ -1,18 +1,28 @@
 ﻿CREATE TABLE eb_locations
 (
-	id integer auto_increment,
-    shortname varchar(25) DEFAULT 'default',
-    longname varchar(200) DEFAULT 'default',
+    id integer auto_increment,
+    shortname text,
+    longname text,
     image text,
     meta_json text,
-    eb_data_id integer,
+    firmcode integer,
     eb_ver_id integer,
+    eb_data_id integer,
     week_holiday1 text,
     week_holiday2 text,
+    parent_id numeric,
+    is_group text,
+    eb_location_types_id integer,
+    eb_created_by integer,
+    eb_created_at datetime,
+    eb_lastmodified_by integer,
+    eb_lastmodified_at datetime,
+    eb_del char(1) DEFAULT 'F',
     CONSTRAINT eb_locations_pkey PRIMARY KEY (id)
 );
 
-
 CREATE INDEX eb_locations_id_idx
     ON eb_locations(id);
-
+   
+CREATE INDEX eb_locations_eb_del_idx
+    ON eb_locations(eb_del);
