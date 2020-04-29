779ed3b2e123b5c193c44da614621db5db49f431
robyabraham <robyabraham@expressbase.com>
2020-04-29 20:35:29 +0530

    DateInNotification

--- Helpers/DateTimeHelper.cs
+++ Helpers/DateTimeHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExpressBase.Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -19,6 +20,22 @@ namespace ExpressBase.Common.Helpers
         public static bool IsBewteenTwoDates(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
         {
             return dateToCheck >= startDate && dateToCheck <= endDate;
+        }
+
+        public static string DateInNotification(this DateTime DateTime, string TimeZoneName)
+        {
+            DateTime = DateTime.ConvertFromUtc(TimeZoneName);
+            TimeSpan span = (DateTime.Now - DateTime);
+            string formatted = string.Empty;
+            if ((int)span.TotalMinutes < 5)
+                formatted = "Just Now";
+            else
+                formatted = string.Format("{0}{1}{2}",
+                span.Duration().Days > 0 ? string.Format("{0}d ", span.Days) : string.Empty,
+                span.Duration().Hours > 0 ? string.Format("{0}h ", span.Hours) : string.Empty,
+                span.Duration().Minutes > 0 ? string.Format("{0}m ", span.Minutes) : string.Empty);
+
+            return formatted;
         }
     }
 }
