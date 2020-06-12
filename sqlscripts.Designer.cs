998d00f4fc2311f44de8b9b0b1273e6e8d376cb2
Dona Jose <donajose@expressbase.com>
2020-06-12 12:54:04 +0000

    Added meeting_slots in mysql n mssql

--- sqlscripts.Designer.cs
+++ sqlscripts.Designer.cs
@@ -2751,6 +2751,43 @@ namespace ExpressBase.Common {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to CREATE TABLE eb_meeting_slots
+        ///(
+        ///	id integer auto_increment,
+        ///	eb_meeting_schedule_id integer,
+        ///	is_approved char(1) DEFAULT &apos;F&apos;,
+        ///	meeting_date date ,
+        ///	time_from datetime,
+        ///	time_to	datetime,
+        ///	eb_created_by integer,
+        ///	eb_created_at datetime DEFAULT now(),
+        ///	eb_lastmodified_by integer,
+        ///	eb_lastmodified_at datetime,
+        ///	eb_del char(1) DEFAULT &apos;F&apos;,
+        ///	CONSTRAINT eb_meeting_slots_pkey PRIMARY KEY (id)
+        ///);
+        ///
+        ///CREATE INDEX eb_meeting_slots_id_idx 
+        ///	ON eb_meeting_slots(id);
+        ///
+        ///CREATE INDEX eb_meeting_slots_eb_d [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string eb_meeting_slots2 {
+            get {
+                return ResourceManager.GetString("eb_meeting_slots2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to -- eb_meeting_slots.
+        /// </summary>
+        public static string eb_meeting_slots3 {
+            get {
+                return ResourceManager.GetString("eb_meeting_slots3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to -- Table: public.eb_meetings
         ///
         ///-- DROP TABLE public.eb_meetings;
