db57deea0a0b681028b37d5d3677037e973d455f
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2020-09-17 11:18:10 +0530

    eb_form_drafts script removed for solving the name issue

--- sqlscripts.Designer.cs
+++ sqlscripts.Designer.cs
@@ -19,7 +19,7 @@ namespace ExpressBase.Common {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class sqlscripts {
@@ -1661,60 +1661,6 @@ namespace ExpressBase.Common {
         public static string eb_files_ref3 {
             get {
                 return ResourceManager.GetString("eb_files_ref3", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to -- Table: public.eb_form_drafts
-        ///
-        ///-- DROP TABLE public.eb_form_drafts;
-        ///
-        ///CREATE TABLE public.eb_form_drafts
-        ///(
-        ///    id SERIAL,
-        ///    title text,
-        ///    form_data_json text,
-        ///    form_ref_id text,
-        ///    form_data_id integer,
-        ///    is_submitted &quot;char&quot; DEFAULT &apos;F&apos;::&quot;char&quot;,
-        ///    eb_loc_id integer,
-        ///    eb_created_by integer,
-        ///    eb_created_at timestamp without time zone,
-        ///    eb_lastmodified_at timestamp without time zone,
-        ///    eb_del &quot;char&quot; DEFAULT &apos;F&apos;::&quot;char&quot;,
-        ///    CONSTRAINT eb_form_drafts_pkey PRIMARY KEY (id [rest of string was truncated]&quot;;.
-        /// </summary>
-        public static string eb_form_drafts {
-            get {
-                return ResourceManager.GetString("eb_form_drafts", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to -- eb_form_drafts.
-        /// </summary>
-        public static string eb_form_drafts1 {
-            get {
-                return ResourceManager.GetString("eb_form_drafts1", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to -- eb_form_drafts.
-        /// </summary>
-        public static string eb_form_drafts2 {
-            get {
-                return ResourceManager.GetString("eb_form_drafts2", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to -- eb_form_drafts
-        ///.
-        /// </summary>
-        public static string eb_form_drafts3 {
-            get {
-                return ResourceManager.GetString("eb_form_drafts3", resourceCulture);
             }
         }
         
