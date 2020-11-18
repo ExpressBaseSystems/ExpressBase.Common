b1538722ab7a41e6965fb1018698a78b9d962da2
Febin Carlos <febincarlos@expressbase.com>
2020-11-18 15:51:42 +0530

    FormMetaDataKeys - modified

--- Constants/FormMetaDataKeys.cs
+++ Constants/FormMetaDataKeys.cs
@@ -2,12 +2,6 @@
 {
     public static class FormMetaDataKeys
     {
-        public const string auth_id = "auth_id";
-        public const string username = "username";
-        public const string verification_required = "verification_required";
-        public const string verify_email = "verify_email";
-        public const string verify_phone = "verify_phone";
-        public const string message = "message";
-        public const string token = "token";
+        public const string signup_user = "signup_user";
     }
 }
