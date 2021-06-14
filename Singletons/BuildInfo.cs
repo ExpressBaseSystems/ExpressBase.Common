6b4a472cc259ed4725c80babf597810a1a7c19b6
Febin Carlos <febincarlos@expressbase.com>
2021-06-14 18:43:49 +0400

    BuildInfo class added

---
+++ Singletons/BuildInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExpressBase.Common.Singletons
+{
+    public sealed class BuildInfo
+    {
+        public static readonly string Version = DateTime.UtcNow.ToString("yMdHms");
+    }
+}
