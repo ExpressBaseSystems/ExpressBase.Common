bf2bdcbc6efc5d91e6f19cadb94f6a42bd626520
Febin Carlos <febincarlos@expressbase.com>
2021-10-29 11:42:48 +0400

    Fn to get logo path - added

---
+++ Helpers/UrlHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Common.Helpers
+{
+    public static class UrlHelper
+    {
+        public static string GetLogoPath(string Host)
+        {
+            if (Host?.Contains("safenetin") == true)
+                return "/images/logo-safe.png";
+            return "/images/EB_Logo-png.png";
+        }
+
+        public static string GetSmallLogoPath(string Host)
+        {
+            if (Host?.Contains("safenetin") == true)
+                return "/images/logo-safe.png";
+            return "/images/EB_Logo.png";
+        }
+    }
+}
