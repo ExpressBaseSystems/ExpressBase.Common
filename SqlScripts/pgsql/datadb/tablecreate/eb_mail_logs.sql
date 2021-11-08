60cfa33b86fb41acad9961098faf20b059f424ac
Chinchu <chinchucvijayan@expressbase.com>
2021-11-08 16:18:01 +0530

    eb_mail_logs script updated

--- SqlScripts/pgsql/datadb/tablecreate/eb_mail_logs.sql
+++ SqlScripts/pgsql/datadb/tablecreate/eb_mail_logs.sql
@@ -6,15 +6,25 @@ CREATE TABLE eb_mail_logs
 (
 	id serial,
     send_to text,
+    send_from text,
+	recepients text
     message_body text,
+    attachmentname text,
+    subject text,
     status text,
+    result text,
     refid text,
+    metadata json,
+    retryof integer,
+    isfallback "char" DEFAULT 'F'::"char",
     con_id integer,
+    req_from text,
     eb_created_by integer,
     eb_created_at timestamp without time zone,
-	eb_del "char" DEFAULT 'F'::"char",
+    eb_del "char" DEFAULT 'F'::"char",
     CONSTRAINT eb_mail_logs_pkey PRIMARY KEY (id)
 );
+
 
 
 -- Index: eb_mail_logs_id_idx
