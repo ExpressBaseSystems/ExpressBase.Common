ae962510632a9d4ac88ef4b06ff5eaf7aedb5167
Dona Jose <donajose@expressbase.com>
2022-03-31 20:26:38 +0530

    FileClient local url

--- ServiceClients/EbStaticFileClient.cs
+++ ServiceClients/EbStaticFileClient.cs
@@ -17,8 +17,8 @@ namespace ExpressBase.Common.ServiceClients
             this.BaseUri = Environment.GetEnvironmentVariable(EnvironmentConstants.EB_STATICFILESERVER_INT_URL);
             this.RefreshTokenUri = Environment.GetEnvironmentVariable(EnvironmentConstants.EB_GET_ACCESS_TOKEN_URL);
 
-            //this.BaseUri = "https://fs.eb-test.xyz";
-            //this.RefreshTokenUri = "https://ss.eb-test.xyz/access-token";
+            //this.BaseUri = "http://localhost:41800";
+            //this.RefreshTokenUri = "http://localhost:41600/access-token";
         }
 
         //public EbStaticFileClient(Container c)
