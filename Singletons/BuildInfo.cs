87990f92eb57c07e8ae72caa090fcdb0b3daba87
Febin Carlos <febincarlos@expressbase.com>
2022-06-10 12:08:35 +0530

    md5 of build version for cxt2js

--- Singletons/BuildInfo.cs
+++ Singletons/BuildInfo.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using ExpressBase.Common.Extensions;
+using System.Reflection;
 
 namespace ExpressBase.Common.Singletons
 {
     public sealed class BuildInfo
     {
-        public static readonly string Version = DateTime.UtcNow.ToString("yMdHms");
+        public static readonly string Md5Version = Assembly.GetExecutingAssembly().GetName().Version.ToString().ToMD5Hash();
     }
 }
