f073240f412f3959912d1ed2c1f308a24b204ec1
Febin Carlos <febincarlos@expressbase.com>
2022-09-28 14:28:03 +0530

    Datagrid - Referential integrity column name expr

--- Objects/TableColumnMeta.cs
+++ Objects/TableColumnMeta.cs
@@ -1,5 +1,6 @@
 ﻿using ExpressBase.Common.Objects;
 using ExpressBase.Common.Structures;
+using ExpressBase.Objects;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -282,6 +283,10 @@ namespace ExpressBase.Common
         public string Title { get; set; }
 
         public string ContainerName { get; set; }
+
+        public EbScript IntegrityColumnExpr { get; set; }//dg
+
+        public string IntegrityColumn { get; set; }//dg
 
         public bool DescOdr { get; set; }//Descending order eb_row_num - datagrid
 
