99249cd11c200fe05c0fff79cbe0ff6ded307a02
Febin Carlos <febincarlos@expressbase.com>
2024-03-02 16:27:04 +0530

    BuildInfo class moved to objects

--- Constants/RedisKeyPrefixConstants.cs
+++ Constants/RedisKeyPrefixConstants.cs
@@ -7,7 +7,7 @@ namespace ExpressBase.Common.Constants
 
         public const string FormSubmissionDataId = "FsdID_{0}_{1}_{2}_{3}";
 
-        public const string EbWebformObj = "EbWebformObj_{0}_{1}";
+        public const string EbWebformObject = "EbWebformObject_{0}_{1}";
 
         public const string EbWebformMd5 = "EbWebformMd5_{0}_{1}";
     }
