d8169c12f0dc054759c4bace3ce044360f72aa57
Jithin P V <jpeevi@hotmail.com>
2025-10-11 02:44:47 +0530

    Added new StringHelper

---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExpressBase.Common.Helpers
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Returns true if the string is neither null, empty, nor whitespace.
+        /// </summary>
+        public static bool HasValue(string input)
+        {
+            return !string.IsNullOrEmpty(input) && !string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
